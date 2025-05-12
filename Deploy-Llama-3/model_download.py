737cba6d0020207f63e69e5db070cc0a15ddc2c9
Hami-8 <1809493351@qq.com>
2025-05-12 22:56:57 +0800

    新增deepseek-r1--Distill-Llama-8B

--- Deploy-Llama-3/model_download.py
+++ Deploy-Llama-3/model_download.py
@@ -5,3 +5,4 @@ import os
 model_dir = snapshot_download('LLM-Research/Meta-Llama-3.1-8B-Instruct', cache_dir='/root/autodl-tmp', revision='master')
 model_dir_2 = snapshot_download('mirror013/mxbai-embed-large-v1', cache_dir='/root/autodl-tmp', revision='master')
 
+# model_dir_3 = snapshot_download('deepseek-ai/DeepSeek-R1-Distill-Llama-8B', cache_dir='/root/autodl-tmp', revision='master')
